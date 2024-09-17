afa837e3d8e89407c331e919bd2a1099e7048705
Marinabsv <146950684+Marinabsv@users.noreply.github.com>
2024-09-17 23:49:40 +0300

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { StrictMode } from 'react';
+//import './styles/index.scss';
+import { App } from './components/app/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [article, setArticle] = useState<ArticleStateType>(defaultArticleState);
-
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': article.fontFamilyOption.value,
-					'--font-size': article.fontSizeOption.value,
-					'--font-color': article.fontColor.value,
-					'--container-width': article.contentWidth.value,
-					'--bg-color': article.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm changedSettings={setArticle} />
-			<Article />
-		</div>
-	);
-};
 
 root.render(
 	<StrictMode>
